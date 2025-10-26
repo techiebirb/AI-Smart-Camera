0d54fb3d7a6e64296823cb0f26cb3b3f4eb14587
techiebirb <73718824+techiebirb@users.noreply.github.com>
2025-10-25 21:18:36 -0400

    Port change

--- mainflask.py
+++ mainflask.py
@@ -59,4 +59,4 @@ def videoplayer():
     return render_template("videoplayer.html", videoid=videoid)
     
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=4040)
